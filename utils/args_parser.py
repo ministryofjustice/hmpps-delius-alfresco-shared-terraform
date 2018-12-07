69a747d116358ba70b356934c59acbed07a03283
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-12-07 11:50:48 +0000

    Removed Nginx ecs

--- utils/args_parser.py
+++ utils/args_parser.py
@@ -7,7 +7,7 @@ def argsParser():
     parser.add_argument('--env', type=str,
                         help='target environment', required=True)
     parser.add_argument('--action', type=str, help='action to perform',
-                        choices=['apply', 'plan', 'test', 'output'], required=True)
+                        choices=['apply', 'plan', 'test', 'output', 'destroy'], required=True)
     parser.add_argument('--component', type=str,
                         help='component to run task on', default='common')
     parser.add_argument('--token', type=str,
