6d2685582d51dbec6f553ac403afcb76c71e29a3
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-04-05 14:54:56 +0100

    Adding sync option v20

--- scripts/s3_copy_contents.sh
+++ scripts/s3_copy_contents.sh
@@ -83,7 +83,7 @@ exit_on_error $? !!
 ALFRESCO_SQL_FILE="alfresco.sql"
 
 echo "------> SYNC DONE"
-aws s3 sync s3://${SRC_S3_BUCKET}/TRN200/Alfresco/alfresco_db_s3_support.sql alfresco_db_support.sql
+aws s3 cp s3://${SRC_S3_BUCKET}/TRN200/Alfresco/alfresco_db_s3_support.sql alfresco_db_support.sql
 
 cat alfresco_db_support.sql
 ls 
