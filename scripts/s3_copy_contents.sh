a54c241d225d8deac4a225d54fbc5ad4e77f2ff9
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-04-05 12:19:30 +0100

    Adding sync option v12

--- scripts/s3_copy_contents.sh
+++ scripts/s3_copy_contents.sh
@@ -74,5 +74,8 @@ exit_on_error $? !!
 aws s3 rm s3://${DEST_S3_BUCKET} --recursive
 exit_on_error $? !!
 
-#aws s3 sync s3://${SRC_S3_BUCKET}/TRN200/Alfresco/contentstore s3://${DEST_S3_BUCKET}/
-#exit_on_error $? !!
+aws s3 sync s3://${SRC_S3_BUCKET}/TRN200/Alfresco/contentstore s3://${DEST_S3_BUCKET}/
+exit_on_error $? !!
+
+aws s3 sync s3://${SRC_S3_BUCKET}/TRN200/Alfresco s3://${DEST_S3_BUCKET}/
+exit_on_error $? !!
