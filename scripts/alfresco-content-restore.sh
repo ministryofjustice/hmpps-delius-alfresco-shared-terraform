38f977344ef025263d3e5e03606ef36d0d02a92c
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2019-08-23 14:33:19 +0100

    Fixes (#139)
    * Refactored folliwing failed jenkins jobs
    
    * Refactored to to delete when syncing

--- scripts/alfresco-content-restore.sh
+++ scripts/alfresco-content-restore.sh
@@ -17,16 +17,14 @@ then
   echo "Run mode set to ${ALF_RESTORE_STATUS}"
   echo "Source bucket: ${CONFIG_BUCKET}"
   echo "Storage bucket: ${ALF_STORAGE_BUCKET}"
-  aws s3 rm s3://${ALF_STORAGE_BUCKET} --recursive
-  aws s3 sync ${CONTENT_STORE_CMD}
-  aws s3 sync ${CONTENT_STORE_DELETED_CMD}
+  aws s3 sync --delete ${CONTENT_STORE_CMD}
+  aws s3 sync --delete ${CONTENT_STORE_DELETED_CMD}
   echo "------> SYNC DONE"
 
 else
   echo "Run mode set to ${ALF_RESTORE_STATUS}, dry-run flags set"
-  aws s3 rm s3://${ALF_STORAGE_BUCKET} --recursive --dryrun
-  aws s3 sync ${CONTENT_STORE_CMD} --dryrun
-  aws s3 sync ${CONTENT_STORE_DELETED_CMD} --dryrun
+  aws s3 sync --delete ${CONTENT_STORE_CMD} --dryrun
+  aws s3 sync --delete ${CONTENT_STORE_DELETED_CMD} --dryrun
   echo "------> DRY RUN SYNC DONE"
 fi
 
