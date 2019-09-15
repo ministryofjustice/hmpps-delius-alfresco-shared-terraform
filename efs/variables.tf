72234d07acd3f113f726a1471b4dbe27dc768934
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-09-15 08:45:11 +0100

    Updated EFS share

--- efs/variables.tf
+++ efs/variables.tf
@@ -4,3 +4,11 @@ variable "region" {}
 variable "remote_state_bucket_name" {
   description = "Terraform remote state bucket name"
 }
+
+variable "alf_backups_config" {
+  type = "map"
+  default = {
+    provisioned_throughput_in_mibps = 10
+    throughput_mode                 = "provisioned"
+  }
+}
