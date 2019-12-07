52632f5b52af42819a93f24fbee21475fd2fc75f
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2019-12-07 18:55:21 +0000

    added region to shell script

--- ami_permissions/data.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "ami" {
-  template = "${file("scripts/data.conf")}"
-
-  vars {
-    image_id = "${local.image_id}"
-  }
-}
--- ami_permissions/main.tf
+++ ami_permissions/main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "ami_update_perms" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids["${local.account_id}"]}"
+    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids["${local.account_id}"]} ${var.region}"
   }
 }
 
@@ -26,7 +26,7 @@ resource "null_resource" "ami_perms" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    command = "aws ec2 describe-image-attribute --image-id ${local.image_id} --attribute launchPermission"
+    command = "aws --region ${var.region} ec2 describe-image-attribute --image-id ${local.image_id} --attribute launchPermission"
   }
   depends_on = ["null_resource.ami_update_perms"]
 }
