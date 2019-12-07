06f978f23859504b0e585d7f2f9a2f06fe4f42de
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2019-12-07 17:11:04 +0000

    Added ami release process (#189)

--- ami_permissions/main.tf
+++ ami_permissions/main.tf
@@ -9,9 +9,29 @@ provider "aws" {
 
 locals {
   account_id = "hmpps-${var.environment_name}"
+  image_id   = "${var.alfresco_asg_props["image_id"]}"
 }
 
-resource "aws_ami_launch_permission" "ami" {
-  image_id   = "${var.alfresco_asg_props["image_id"]}"
-  account_id = "${var.alf_account_ids["${local.account_id}"]}"
+# resource "aws_ami_launch_permission" "ami" {
+#   image_id   = "${var.alfresco_asg_props["image_id"]}"
+#   account_id = "${var.alf_account_ids["${local.account_id}"]}"
+# }
+
+resource "null_resource" "ami_update_perms" {
+  triggers = {
+    ami_sha = "${sha1(data.template_file.ami.rendered)}"
+  }
+  provisioner "local-exec" {
+    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids["${local.account_id}"]}"
+  }
+}
+
+resource "null_resource" "ami_perms" {
+  triggers = {
+    ami_sha = "${sha1(data.template_file.ami.rendered)}"
+  }
+  provisioner "local-exec" {
+    command = "aws ec2 describe-image-attribute --image-id ${local.image_id} --attribute launchPermission"
+  }
+  depends_on = ["null_resource.ami_update_perms"]
 }
