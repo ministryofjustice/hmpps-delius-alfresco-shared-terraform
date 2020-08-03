6b96dd95be78460d3c9ccc94bf9ca1a8b4a90483
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-08-03 12:56:12 +0100

    ami permissions done

--- ami_permissions/main.tf
+++ ami_permissions/main.tf
@@ -1,32 +1,29 @@
 terraform {
-  backend "s3" {}
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 1.16"
+  backend "s3" {
+  }
 }
 
 locals {
   account_id = "hmpps-${var.environment_name}"
-  image_id   = "${var.alfresco_asg_props["image_id"]}"
+  image_id   = var.alfresco_asg_props["image_id"]
 }
 
 resource "null_resource" "ami_update_perms" {
   triggers = {
-    always_run = "${timestamp()}"
+    always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids["${local.account_id}"]} ${var.region}"
+    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids[local.account_id]} ${var.region}"
   }
 }
 
 resource "null_resource" "ami_perms" {
   triggers = {
-    always_run = "${timestamp()}"
+    always_run = timestamp()
   }
   provisioner "local-exec" {
     command = "aws --region ${var.region} ec2 describe-image-attribute --image-id ${local.image_id} --attribute launchPermission"
   }
-  depends_on = ["null_resource.ami_update_perms"]
+  depends_on = [null_resource.ami_update_perms]
 }
+
--- ami_permissions/variables.tf
+++ ami_permissions/variables.tf
@@ -1,19 +1,21 @@
-variable "region" {}
+variable "region" {
+}
 
 variable "alfresco_asg_props" {
-  type = "map"
+  type = map(string)
   default = {
     image_id = "" # used for updating ami launch permissions
   }
 }
 
 variable "environment_name" {
-  type = "string"
+  type = string
 }
 
 variable "alf_account_ids" {
-  type = "map"
+  type = map(string)
   default = {
     hmpps-alfresco-dev = "563502482979"
   }
 }
+
