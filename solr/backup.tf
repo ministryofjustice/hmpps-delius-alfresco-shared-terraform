12b39e580209549a8d4282a6b6c8bd876b14747c
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-08-21 16:41:48 +0100

    solr done

--- solr/backup.tf
+++ solr/backup.tf
@@ -1,20 +1,26 @@
 data "template_file" "solr_backup" {
-  template = "${file("../policies/backup_assume_role.tpl")}"
+  template = file("../policies/backup_assume_role.tpl")
   vars     = {}
 }
+
 resource "aws_iam_role" "solr_backup" {
   name               = "${local.common_name}-ebs-bkup"
-  assume_role_policy = "${data.template_file.solr_backup.rendered}"
+  assume_role_policy = data.template_file.solr_backup.rendered
 }
 
 resource "aws_iam_role_policy_attachment" "solr_backup" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-  role       = "${aws_iam_role.solr_backup.name}"
+  role       = aws_iam_role.solr_backup.name
 }
 
 resource "aws_backup_vault" "solr_backup" {
   name = "${local.common_name}-ebs-bkup"
-  tags = "${merge(local.tags, map("Name", "${local.common_name}-ebs-bkup"))}"
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.common_name}-ebs-bkup"
+    },
+  )
 }
 
 resource "aws_backup_plan" "solr_backup" {
@@ -22,26 +28,32 @@ resource "aws_backup_plan" "solr_backup" {
 
   rule {
     rule_name         = "${local.common_name}-ebs-bkup"
-    target_vault_name = "${aws_backup_vault.solr_backup.name}"
-    schedule          = "${var.alf_solr_config["schedule"]}"
+    target_vault_name = aws_backup_vault.solr_backup.name
+    schedule          = var.alf_solr_config["schedule"]
 
-    lifecycle = {
-      cold_storage_after = "${var.alf_solr_config["cold_storage_after"]}"
-      delete_after       = "${var.alf_solr_config["delete_after"]}"
+    lifecycle {
+      cold_storage_after = var.alf_solr_config["cold_storage_after"]
+      delete_after       = var.alf_solr_config["delete_after"]
     }
   }
 
-  tags = "${merge(local.tags, map("Name", "${local.common_name}-ebs-bkup"))}"
+  tags = merge(
+    local.tags,
+    {
+      "Name" = "${local.common_name}-ebs-bkup"
+    },
+  )
 }
 
 resource "aws_backup_selection" "solr_backup" {
-  iam_role_arn = "${aws_iam_role.solr_backup.arn}"
+  iam_role_arn = aws_iam_role.solr_backup.arn
   name         = "${local.common_name}-ebs-bkup"
-  plan_id      = "${aws_backup_plan.solr_backup.id}"
+  plan_id      = aws_backup_plan.solr_backup.id
 
   selection_tag {
     type  = "STRINGEQUALS"
-    key   = "${var.alf_solr_config["snap_tag"]}"
+    key   = var.alf_solr_config["snap_tag"]
     value = "1"
   }
 }
+
--- solr/cloudwatch.tf
+++ solr/cloudwatch.tf
@@ -2,10 +2,11 @@
 # CloudWatch
 ###############################################
 module "create_loggroup" {
-  source                   = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//cloudwatch//loggroup"
-  log_group_path           = "${local.short_environment_identifier}"
+  source                   = "../modules/cloudwatch/loggroup"
+  log_group_path           = local.short_environment_identifier
   loggroupname             = "solr"
-  cloudwatch_log_retention = "${var.alf_cloudwatch_log_retention}"
-  kms_key_id               = "${local.logs_kms_arn}"
-  tags                     = "${local.tags}"
+  cloudwatch_log_retention = var.alf_cloudwatch_log_retention
+  kms_key_id               = local.logs_kms_arn
+  tags                     = local.tags
 }
+
