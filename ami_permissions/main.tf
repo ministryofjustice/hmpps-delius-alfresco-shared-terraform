333c61b0351c5cc71096164d1fdb51ad1450a76b
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-08-26 17:31:06 +0100

    Fixed alfresco ami dev

--- ami_permissions/main.tf
+++ ami_permissions/main.tf
@@ -4,8 +4,9 @@ terraform {
 }
 
 locals {
-  account_id = "hmpps-${var.environment_name}"
-  image_id   = var.alfresco_asg_props["image_id"]
+  account_alias = "hmpps-${var.environment_name}"
+  image_id      = var.alfresco_asg_props["image_id"]
+  account_id    = lookup(var.alf_account_ids, local.account_alias)
 }
 
 resource "null_resource" "ami_update_perms" {
@@ -13,7 +14,7 @@ resource "null_resource" "ami_update_perms" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "sh scripts/update_ami.sh ${local.image_id} ${var.alf_account_ids[local.account_id]} ${var.region}"
+    command = "sh scripts/update_ami.sh ${local.image_id} ${local.account_id} ${var.region}"
   }
 }
 
@@ -26,4 +27,3 @@ resource "null_resource" "ami_perms" {
   }
   depends_on = [null_resource.ami_update_perms]
 }
-
