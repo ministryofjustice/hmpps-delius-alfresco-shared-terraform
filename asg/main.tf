fbe0b07ac61a623ddc9794ee92d980043560bd88
Don Nyambudzi <Don.Nyambudzi@madiro.co.uk>
2021-05-18 16:51:43 +0100

    Patch/update python deps (#317)
    * Updated python deps
    
    * Removed unused data source

--- asg/main.tf
+++ asg/main.tf
@@ -8,18 +8,6 @@ terraform {
 ####################################################
 # DATA SOURCE MODULES FROM OTHER TERRAFORM BACKENDS
 ####################################################
-#-------------------------------------------------------------
-### Getting the current vpc
-#-------------------------------------------------------------
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    bucket = var.remote_state_bucket_name
-    key    = "vpc/terraform.tfstate"
-    region = var.region
-  }
-}
 
 #-------------------------------------------------------------
 ### Getting the common details
