4489e744e1b895a2e75a7578eb52f9aebf5c54b0
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-10-05 19:31:17 +0100

    Remapped content security group

--- alfresco-read-only/output.tf
+++ alfresco-read-only/output.tf
@@ -2,7 +2,7 @@ output "info" {
   value = {
     loggroup_name         = module.create_loggroup.loggroup_name
     app_secuirty_group    = aws_security_group.app.id
-    access_security_group = aws_security_group.access.id
+    access_security_group = local.access_group_id
     end_point             = format("%s:%s", local.internal_private_dns_host, local.target_group_port)
     storage_bucket_name   = local.storage_bucket_name
   }
