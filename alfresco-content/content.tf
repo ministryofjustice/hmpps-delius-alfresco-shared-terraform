419faa21d4f7a9ffdd315d8ec9f303db15ebb74d
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-10-06 17:46:46 +0100

    Reverted grace period change to 300

--- alfresco-content/content.tf
+++ alfresco-content/content.tf
@@ -12,7 +12,7 @@ module "ecs_service" {
     namespace_id          = local.ecs_cluster_namespace_id
     fluentbit_s3_arn      = format("%s/%s", local.config_bucket_arn, local.fluentbit_s3_path)
     config_bucket_arn     = local.config_bucket_arn
-    grace_period          = "180"
+    grace_period          = "300"
   }
   security_groups = [
     aws_security_group.app.id,
