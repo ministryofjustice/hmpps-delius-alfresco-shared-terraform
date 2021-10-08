5076366056babf7f8824df30883c1266668d087f
Chamanga Mupanguri <Don.Nyambudzi@madiro.co.uk>
2021-10-08 15:46:37 +0100

    Update access_groups.tf (#341)
    * Update access_groups.tf
    
    Removed unused security groups
    
    * Add new clients group

--- security-groups/access_groups.tf
+++ security-groups/access_groups.tf
@@ -14,6 +14,22 @@ resource "aws_security_group" "content" {
   }
 }
 
+resource "aws_security_group" "content_clients" {
+  name        = format("%s-content-clients", local.common_name)
+  description = format("security group for %s-content-traffic", local.common_name)
+  vpc_id      = local.vpc_id
+  tags = merge(
+    local.tags,
+    {
+      "Name" = format("%s-content-clients", local.common_name)
+    },
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_security_group" "share" {
   name        = format("%s-share", local.common_name)
   description = format("security group for %s-share-traffic", local.common_name)
@@ -30,30 +46,14 @@ resource "aws_security_group" "share" {
   }
 }
 
-resource "aws_security_group" "transform" {
-  name        = format("%s-transform", local.common_name)
-  description = format("security group for %s-transform-traffic", local.common_name)
+resource "aws_security_group" "share_clients" {
+  name        = format("%s-share-clients", local.common_name)
+  description = format("security group for %s-share-clients-traffic", local.common_name)
   vpc_id      = local.vpc_id
   tags = merge(
     local.tags,
     {
-      "Name" = format("%s-transform", local.common_name)
-    },
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group" "search" {
-  name        = format("%s-search", local.common_name)
-  description = format("security group for %s-search-traffic", local.common_name)
-  vpc_id      = local.vpc_id
-  tags = merge(
-    local.tags,
-    {
-      "Name" = format("%s-search", local.common_name)
+      "Name" = format("%s-share-clients", local.common_name)
     },
   )
 
@@ -64,7 +64,7 @@ resource "aws_security_group" "search" {
 
 output "alf_access_groups" {
   value = {
-    content = aws_security_group.content.id
-    share   = aws_security_group.share.id
+    content = aws_security_group.content_clients.id
+    share   = aws_security_group.share_clients.id
   }
 }
