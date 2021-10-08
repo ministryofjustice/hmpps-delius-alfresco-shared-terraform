2bdcf9ba44984fc52d81e5ef6e1822ba849684c4
Chamanga Mupanguri <Don.Nyambudzi@madiro.co.uk>
2021-10-08 16:57:14 +0100

    Old access sgs removed (#342)

--- security-groups/access_groups.tf
+++ security-groups/access_groups.tf
@@ -1,19 +1,3 @@
-resource "aws_security_group" "content" {
-  name        = format("%s-content", local.common_name)
-  description = format("security group for %s-content-traffic", local.common_name)
-  vpc_id      = local.vpc_id
-  tags = merge(
-    local.tags,
-    {
-      "Name" = format("%s-content", local.common_name)
-    },
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_security_group" "content_clients" {
   name        = format("%s-content-clients", local.common_name)
   description = format("security group for %s-content-traffic", local.common_name)
@@ -22,22 +6,6 @@ resource "aws_security_group" "content_clients" {
     local.tags,
     {
       "Name" = format("%s-content-clients", local.common_name)
-    },
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_security_group" "share" {
-  name        = format("%s-share", local.common_name)
-  description = format("security group for %s-share-traffic", local.common_name)
-  vpc_id      = local.vpc_id
-  tags = merge(
-    local.tags,
-    {
-      "Name" = format("%s-share", local.common_name)
     },
   )
 
