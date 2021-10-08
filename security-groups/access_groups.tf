9cf91bb8beb9b8c040d7a4fcf3ecfdf69cd23737
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-10-08 15:28:51 +0100

    Add new clients group

--- security-groups/access_groups.tf
+++ security-groups/access_groups.tf
@@ -14,6 +14,22 @@ resource "aws_security_group" "content" {
   }
 }
 
+resource "aws_security_group" "content_clients" {
+  name        = format("%s-content-clients", local.common_name)
+  description = format("security group for %s-content-traffic", local.common_name)
+  vpc_id      = local.vpc_id
+  tags = merge(
+    local.tags,
+    {
+      "Name" = format("%s-content-clients", local.common_name)
+    },
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_security_group" "share" {
   name        = format("%s-share", local.common_name)
   description = format("security group for %s-share-traffic", local.common_name)
@@ -30,9 +46,25 @@ resource "aws_security_group" "share" {
   }
 }
 
+resource "aws_security_group" "share_clients" {
+  name        = format("%s-share-clients", local.common_name)
+  description = format("security group for %s-share-clients-traffic", local.common_name)
+  vpc_id      = local.vpc_id
+  tags = merge(
+    local.tags,
+    {
+      "Name" = format("%s-share-clients", local.common_name)
+    },
+  )
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 output "alf_access_groups" {
   value = {
-    content = aws_security_group.content.id
-    share   = aws_security_group.share.id
+    content = aws_security_group.content_clients.id
+    share   = aws_security_group.share_clients.id
   }
 }
