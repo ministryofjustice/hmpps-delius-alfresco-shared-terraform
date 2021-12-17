af1c6b08f43914ea185f28b43c386ed5c2e1a710
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-12-17 15:38:41 +0000

    Updated env-configs version

--- ecs_cluster/security_groups.tf
+++ ecs_cluster/security_groups.tf
@@ -3,20 +3,20 @@ resource "aws_security_group" "ecs_host_sg" {
   description = "Alfresco ECS Cluster Hosts Security Group"
   vpc_id      = local.vpc_id
   tags = merge(
-    local.tags, 
+    local.tags,
     { Name = "${local.common_name}-host" }
   )
 }
 
 locals {
-    ecs_security_groups = {
-        host = aws_security_group.ecs_host_sg.id
-        efs = aws_security_group.ecs_efs_sg.id
-    }
+  ecs_security_groups = {
+    host = aws_security_group.ecs_host_sg.id
+    efs  = aws_security_group.ecs_efs_sg.id
+  }
 }
 
 resource "aws_security_group_rule" "all_out" {
-  for_each = local.ecs_security_groups
+  for_each          = local.ecs_security_groups
   security_group_id = each.value
   type              = "egress"
   from_port         = 0
@@ -31,7 +31,7 @@ resource "aws_security_group" "ecs_efs_sg" {
   description = "Alfresco ECS Cluster efs Security Group"
   vpc_id      = local.vpc_id
   tags = merge(
-    local.tags, 
+    local.tags,
     { Name = "${local.common_name}-efs" }
   )
 }
@@ -44,4 +44,14 @@ resource "aws_security_group_rule" "vpn_ssh" {
   protocol          = "tcp"
   cidr_blocks       = data.terraform_remote_state.common.outputs.vpn_info["source_cidrs"]
   description       = "vpn tunnelling"
+}
+
+resource "aws_security_group_rule" "bastion_ssh" {
+  security_group_id = local.ecs_security_groups["host"]
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = data.terraform_remote_state.common.outputs.bastion_cidr_ranges
+  description       = "bastion access"
 }
