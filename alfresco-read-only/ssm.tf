1b5019f1f3121a834f443cc7b03df70f01234559
adrianweetman <adrian.weetman@digital.justice.gov.uk>
2022-03-29 11:26:28 +0100

    Added in config options for download cleaner

--- alfresco-read-only/ssm.tf
+++ alfresco-read-only/ssm.tf
@@ -5,26 +5,28 @@ resource "aws_ssm_parameter" "config" {
   value = templatefile(
     "${path.module}/templates/config/runtime.conf",
     {
-      db_name           = data.terraform_remote_state.rds.outputs.rds_creds["db_name"]
-      db_user           = data.aws_ssm_parameter.db_user.value
-      db_password       = data.aws_ssm_parameter.db_password.value
-      db_endpoint       = data.terraform_remote_state.rds.outputs.info["address"]
-      memory            = tonumber(local.alfresco_ro_content_props["memory"])
-      solr_host         = local.internal_private_dns_host
-      solr_port         = local.solr_port
-      share_host        = local.internal_private_dns_host
-      share_port        = 8070
-      alfresco_host     = format("http://%s", local.internal_private_dns_host)
-      base_url_overwrite = format("http://%s:%s", local.internal_private_dns_host, local.target_group_port)
-      alfresco_port     = local.target_group_port
-      alfresco_protocol = "http"
-      transform_host    = local.internal_private_dns_host
-      transform_port    = 8090
-      s3_bucket_name    = local.storage_bucket_name
-      s3_bucket_region  = local.region
-      cache_location    = local.cache_location
-      server_allowWrite = false
-      db_schema_update  = false
+      db_name                          = data.terraform_remote_state.rds.outputs.rds_creds["db_name"]
+      db_user                          = data.aws_ssm_parameter.db_user.value
+      db_password                      = data.aws_ssm_parameter.db_password.value
+      db_endpoint                      = data.terraform_remote_state.rds.outputs.info["address"]
+      memory                           = tonumber(local.alfresco_ro_content_props["memory"])
+      solr_host                        = local.internal_private_dns_host
+      solr_port                        = local.solr_port
+      share_host                       = local.internal_private_dns_host
+      share_port                       = 8070
+      alfresco_host                    = format("http://%s", local.internal_private_dns_host)
+      base_url_overwrite               = format("http://%s:%s", local.internal_private_dns_host, local.target_group_port)
+      alfresco_port                    = local.target_group_port
+      alfresco_protocol                = "http"
+      transform_host                   = local.internal_private_dns_host
+      transform_port                   = 8090
+      s3_bucket_name                   = local.storage_bucket_name
+      s3_bucket_region                 = local.region
+      cache_location                   = local.cache_location
+      server_allowWrite                = false
+      db_schema_update                 = false
+      download_cleaner_repeat_delay_ms = 315569520000 # Set for a long time to effectively disable
+      download_cleaner_start_delay_ms  = 315569520000 # Set for a long time to effectively disable
     }
   )
   tags = local.tags
