a01aa42daf267e6062e8ac63a2d1510d4cc1bfcb
Seb Norris <sebastian@automationlogic.com>
2023-07-06 11:57:59 +0100

    remove duplicate resource

--- monitoring/dashboards.tf
+++ monitoring/dashboards.tf
@@ -2,8 +2,3 @@ resource "aws_cloudwatch_dashboard" "alfresco_ecs" {
   dashboard_name = "alfresco_ecs"
   dashboard_body = data.template_file.alfresco_ecs.rendered
 }
-
-resource "aws_cloudwatch_dashboard" "alfresco_ecs" {
-  dashboard_name = "alfresco_ecs"
-  dashboard_body = data.template_file.alfresco_ecs.rendered
-}
