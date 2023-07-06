5eadcb5bc173a7de1ac0a429a206f0cf6f96e422
Seb Norris <sebastian@automationlogic.com>
2023-07-06 14:35:30 +0100

    cpu and memory ecs dashboard

--- monitoring/dashboards.tf
+++ monitoring/dashboards.tf
@@ -2,3 +2,10 @@ resource "aws_cloudwatch_dashboard" "alfresco_ecs" {
   dashboard_name = "alfresco_ecs"
   dashboard_body = data.template_file.alfresco_ecs.rendered
 }
+
+data "template_file" "alfresco_ecs" {
+  template = "${file("${path.module}/templates/dashboards/alfresco_ecs.tpl")}"
+  vars = {
+    environment_name = "dev"
+  }
+}
